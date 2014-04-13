325cfb405e1cd9c4b2de577c6c82bcbb8df10391
jtomli03 <jotom1000@gmail.com>
2014-04-13 18:14:33 +0100

    launch() method added to QuizGameImpl
    to launch the server; note: previously in main method.

--- QuizGameImpl.java
+++ QuizGameImpl.java
@@ -141,30 +141,33 @@ public int addQuiz(Quiz quiz) throws RemoteException{
 	return quiz.getId();
 }
 
-
+private void launch(){
+	//1. If there is no security manager, start one
+	if (System.getSecurityManager() == null) {
+	System.setSecurityManager(new RMISecurityManager());
+	}
+	try {
+	// 2. Create the registry if there is not one
+	LocateRegistry.createRegistry(1099);
+	// 3. Create the server object
+	QuizGame server = new QuizGameImpl();
+	// 4. Register (bind) the server object on the registy.
+	// The registry may be on a different machine
+	String registryHost = "//localhost/";
+	String serviceName = "QuizMaster";
+	Naming.rebind(registryHost + serviceName, server);
+	System.out.println("Working");
+	} catch (MalformedURLException ex) {
+	ex.printStackTrace();
+	} catch (RemoteException ex) {
+	ex.printStackTrace();
+	}	
+}
 
 //Main method
 	public static void main (String[] args){
-		//1. If there is no security manager, start one
-		if (System.getSecurityManager() == null) {
-		System.setSecurityManager(new RMISecurityManager());
-		}
-		try {
-		// 2. Create the registry if there is not one
-		LocateRegistry.createRegistry(1099);
-		// 3. Create the server object
-		QuizGame server = new QuizGameImpl();
-		// 4. Register (bind) the server object on the registy.
-		// The registry may be on a different machine
-		String registryHost = "//localhost/";
-		String serviceName = "QuizMaster";
-		Naming.rebind(registryHost + serviceName, server);
-		System.out.println("Working");
-		} catch (MalformedURLException ex) {
-		ex.printStackTrace();
-		} catch (RemoteException ex) {
-		ex.printStackTrace();
-		}
+		QuizGameImpl quizGame = new QuizGameImpl();
+		quizGame.launch();
 
 	}
 	
