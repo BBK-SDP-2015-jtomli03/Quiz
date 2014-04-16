bf6f5dc34e2f34b3811ab3bfe10d056af6c3959c
jtomli03 <jotom1000@gmail.com>
2014-04-16 15:36:16 +0100

    Full javadoc added to Player and PlayerImpl

--- Player.java
+++ Player.java
@@ -1,24 +1,24 @@
 /**
-* Implements a player
+* A Player is someone who wants to play a game. A player has a userName and a unique player ID.
 */ 
 public interface Player{
 	
 /**
 * Returns a unique player ID number
 *
-* @return int a unique player ID number
+* @return the players unique ID number
 */
 int getId();
 
 /**
 *Sets the player ID number
 *
-* @param the id number to be set for this player
+* @param id the unique id number to be set for this player
 */
 void setId(int id);
 
 /**
-*Gets the players username
+*Gets the players userName
 *
 * @return the players username
 */
--- PlayerImpl.java
+++ PlayerImpl.java
@@ -1,29 +1,52 @@
 import java.io.Serializable;
 
+/**
+* Represents a Player of a game.
+*
+* @author Jo Tomlinson
+*/
 public class PlayerImpl implements Player, Serializable{
 	private static final long serialVersionUID = 1;
 	String userName = "";
 	int playerId = 0;
 
-	public PlayerImpl(String userName){
-		this.userName = userName;
-	}
+/**
+* Creates a new PlayerImpl object
+*
+* @param userName the players username
+*/
+public PlayerImpl(String userName){
+	this.userName = userName;
+}
 
-//Gets the player ID number
+/**
+* Returns a unique player ID number
+*
+* @return the players unique ID number
+*/
 @Override
 public int getId(){
 	return this.playerId;
 }
 
-//Sets the player ID number
+/**
+*Sets the player ID number
+*
+* @param id the unique id number to be set for this player
+*/
 @Override
 public void setId(int id){
 	this.playerId = id;
 }
 
-//gets the userName
+/**
+*Gets the players userName
+*
+* @return the players username
+*/
 @Override
 public String getUserName(){
 	return userName;
 }
+
 }
