b18073c6665406b066aaa8ebf4c92085325bc5e2
jtomli03 <jotom1000@gmail.com>
2014-04-16 17:51:30 +0100

    Full javadoc added for Score and ScoreImpl

--- Score.java
+++ Score.java
@@ -1,29 +1,30 @@
 import java.lang.Cloneable;
 
 /**
-* An interface to implement scores
+* An interface to implement a Score. A Score object has a players unique ID number and the corresponding players score.
 */
 public interface Score{
 	
 
 /**
-* Gets the players score
+* Gets the players actual overall score.
 *
-*@return the players score
-*/	
+*@return int the players score
+*/
 public int getPlayerScore();
 
 /**
-* Gets the players ID number
+* Gets the players unique ID number corresponding to a score
 *
-*@return the players ID number
+*@return the players unique ID number
 */
 public int getPlayerId();
 
 /**
-* Gets the players ID number
+* Clones a Score 
 *
-*@return the players ID number
+* @return an Object (which then needs to be downcast into a Score object)
+* @throws CloneNotSupportedException if an object cannot be cloned.
 */
 Object clone() throws CloneNotSupportedException;
 
--- ScoreImpl.java
+++ ScoreImpl.java
@@ -2,38 +2,78 @@ import java.lang.ClassCastException;
 import java.io.Serializable;
 import java.lang.Cloneable;
 
+/**
+* Represents a Score. A Score has variables playerId (the players ID number) and the 
+* corresponding playerScore (the int value of the players score). A Score is Comparable,
+* Serializable, and Cloneable.
+*
+* @author Jo Tomlinson
+*/
 public class ScoreImpl implements Score, Comparable<Score>, Serializable, Cloneable{
 	private static final long serialVersionUID = 1;
 	int playerId = 0;
 	int playerScore = 0;
 
-	public ScoreImpl(int playerId, int playerScore){
-		this.playerId = playerId;
-		this.playerScore = playerScore;
-	}
+/**
+* Creates a new ScoreImpl object and sets the playerId and playerScore.
+*
+* @param playerId the players unique ID number corresponding to the score.
+* @param playerScore the players actual overall score.
+* 
+*/
+public ScoreImpl(int playerId, int playerScore){
+	this.playerId = playerId;
+	this.playerScore = playerScore;
+}
 
-	public ScoreImpl(){
+/**
+* Creates a new ScoreImpl object.
+*/
+public ScoreImpl(){
 		
-	}
+}
 
+/**
+* Gets the players actual overall score.
+*
+*@return int the players score
+*/	
 @Override
 	public int getPlayerScore(){
 		return playerScore;
 	}
 
+/**
+* Gets the players unique ID number corresponding to a score
+*
+*@return the players unique ID number
+*/
 @Override
 	public int getPlayerId(){
 		return playerId;
 	}
 
+/**
+* Clones a Score 
+*
+* @return an Object (which then needs to be downcast into a Score object)
+* @throws CloneNotSupportedException if an object cannot be cloned.
+*/
 @Override
 public Object clone() throws CloneNotSupportedException{
 	ScoreImpl cloned = (ScoreImpl)super.clone();
 	return cloned;
 }
 
-
-//compares the players scores
+/**
+* Method to compare Score objects according to the playerScore field.
+*
+* @param the Score to be compared to this.Score
+* @return int 0 if this.Score's playerScore is equal to the @param playerScore
+* @return int -1 if this.Score's playerScore is greater than the @param playerScore
+* @return int 1 if this.Score's playerScore is less than the @param playerScore
+* @throws ClassCastException if you try to sort a list whose elements cannot be compared to one another.
+*/
 @Override
 	public int compareTo(Score score) throws ClassCastException{
 		if (!(score instanceof Score)) throw new ClassCastException("A Score object expected.");
